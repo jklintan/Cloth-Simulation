71721aa4b49e00e4fc96454069d2bfea6bf16231
Josefine Klintberg <josefine.klintberg@hotmail.com>
2020-02-06 11:53:28 +0100

    Add simulation options
    Added user choice of simulation type and changing what part of the cloth is fixed

--- Matlab/Point Spring/main.m
+++ Matlab/Point Spring/main.m
@@ -5,6 +5,9 @@
 %This program is a simulation of a cloth based on a
 %mass-spring-damper method.
 
+%Created by Josefine Klintberg, Julius Halldan, Olivia Enroth, Elin
+%Karlsson
+
 %Based on the code by Auralius Manurung
 %Copyright (c) 2016, All rights reserved.
 
@@ -26,15 +29,37 @@ ylabel('meter');
 
 % Parameters, note that stiffness > damping > mass
 % Euler 40, 8, 0.1
-% Verlet (row, col = 10, 20) 40, 12, 0.1 = good with fixed top
-% Verlet (row, col = 10, 20) 100, 14, 0.2 = good with fixed middle
+sim = 1; %1 = top row, 2 = one point, 3 = table
 row =10;
-col = 10;
-stiffness = 40;           % N/m
-damping = 12;            % Ns/m
-mass = 0.1;              % Kg
+col = 20;
 ts = 0.001;              % Seconds
 timelim = 10000;
+distanceNodes = 0.02;    % Meter
+stiffness = 20;           % N/m
+damping = 4;            % Ns/m
+mass = 0.2;              % Kg
+
+if(sim == 1)
+    stiffness = 40;           % N/m
+    damping = 12;            % Ns/m
+end
+
+if(sim == 2)
+    col = 8;
+    row = 10;
+    stiffness = 80;           % N/m 
+    damping = 12;            % Ns/m %For showcase, use damping 2 and show spring force
+    m = 0.1;
+end
+
+if(sim == 3)
+    col = 10;
+    stiffness = 20;           % N/m
+    damping = 4;            % Ns/m
+    mass = 0.2;              % Kg
+end
+
+
 
 % Build the nodes and the canvas
 nodes.row = row;
@@ -42,9 +67,9 @@ nodes.col = col;
 
 for c = 1: col
     for r = 1 : row
-        node(r,c).initalPos = [(c - 1) / 100 (r - 1) / 100 ]; % 1 cm step
+        node(r,c).initalPos = [(c - 1)*distanceNodes (r - 1)*distanceNodes ]; % 1 cm step
         node(r,c).pos = node(r,c).initalPos;
-        node(r,c).pos_old = node(r,c).pos;
+        node(r,c).pos_old = node(r,c).pos; % Used for Verlet integration
         node(r,c).acc = [0 0];
         node(r,c).vel = [0 0];
         node(r,c).force_ext = [0 0];
@@ -53,17 +78,36 @@ for c = 1: col
         %if (r == floor(row/2) && c == floor(col/2))
         %    node(r,c).isFixed = 1;
         % Set top row as fixed
-        if (r == 1)
-            node(r,c).isFixed = 1;
-        % Set middle row as fixed
-        %if (r == floor(row/2))
-        %    node(r,c).isFixed = 1;
-        % Set top two corners as fixed
-        %if (r == 1 && c == 1 || (r == 1 && c == col) )
-        %   node(r,c).isFixed = 1;
-        else
-            node(r,c).isFixed = 0;
+        if(sim == 1)
+            if (r == 1)
+                node(r,c).isFixed = 1;
+            else
+                node(r,c).isFixed = 0;
+            end
         end
+        
+        % Set only one point
+        if(sim == 2)
+            if (r == 1 && c == 1)
+                node(r,c).isFixed = 1;
+            else
+                node(r,c).isFixed = 0;
+            end
+        end
+
+        % Set middle square as fixed
+        if(sim == 3)
+            if ((r <= floor(row/2)+3 && r >= floor(row/2)-3 ) && (c <= floor(col/2)+3 && c >= floor(col/2)-2 ))
+                  node(r,c).isFixed = 1;
+            else
+                node(r,c).isFixed = 0;
+            end
+        end
+
+        % Set corners as fixed
+        %if (r == 1 && c == 1 || (r == 1 && c == col) || (r == row && c == 1) || (r == row && c == col) )
+        %   node(r,c).isFixed = 1;
+
     end
 end
 
@@ -83,9 +127,14 @@ canvas_max = max(plot);
 range = canvas_max - canvas_min;
 
 xlim([canvas_min(1)-range(1) canvas_max(1)+range(1)])
-ylim([canvas_min(2)-range(2)*nodes.row canvas_max(2)])
-%ylim([-0.8 canvas_max(2)]) %For fixed middle
-
+if(sim == 1)
+    ylim([canvas_min(2)-range(2)*nodes.row canvas_max(2)])
+elseif(sim == 2)
+    ylim([-1.2 canvas_max(2)]) %For fixed middle
+elseif(sim == 3)
+    ylim([-0.4 0.4]) %For fixed square
+end
+%view([0 -0.2 0.2])
 plotwindow = drawNodes(S, plot, nodes, 0);
 
 % Main loop
@@ -94,12 +143,8 @@ for i = 0 : timelim
     %Update the position of the nodes
     nodes = updateNode(nodes, mass, stiffness, damping, ts);
     
-    %Do not update every timestep (for efficiency)
-    if mod(i, 10) == 0
-        plotwindow = drawNodes(S, plotwindow, nodes, ts);
-       
-        
-    end
+    %Draw the nodes in the plot
+    plotwindow = drawNodes(S, plotwindow, nodes, ts);
 end
 
 %% Draw the nodes
