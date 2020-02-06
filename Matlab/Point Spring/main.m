ca7011f06caa7b9f059dc2cb9dbfabfbb8df26f6
Julius Halldan <julha230@student.liu.se>
2020-02-06 11:51:15 +0100

    DET FUCKING FUNKAR YESSS WOHOOO

--- Matlab/Point Spring/main.m
+++ Matlab/Point Spring/main.m
@@ -1,20 +1,20 @@
-clear;
+clear all;
 clc;
 close('all');
 
 plane_size = [10,10];                %Storleken på planet som ska skapas
-z = peaks(plane_size(1));            %Test-yta som är fixed (Bra för att prova mesh-funktionen)
+z = ones(plane_size(1));            %Test-yta som är fixed (Bra för att prova mesh-funktionen)
 
-length = 3;                       %Simulationstid i sekunder
-dt = 0.1;                        %Tidsteg i sekunder
+length = 5;                       %Simulationstid i sekunder
+dt = 0.001;                        %Tidsteg i sekunder
 t = 0:dt/length:length;           %Tidssamples
 n = numel(t);                     %Antalet tidssamples
-m = 1/plane_size(1);              %Partikelmasssa
-ks = 100;                         %Fjäderkonstant
-kd = ks/10; 
+m = 20/plane_size(1);              %Partikelmasssa
+ks = 2000;                         %Fjäderkonstant
+kd = 8; 
 plane = point.empty;              %Skapa ett plan
 R = 1;
-
+perspective = [-10 10];
 
 for i = 1:plane_size(1)
     for j = 1:plane_size(2)
@@ -30,9 +30,9 @@ for step = 2:n
     plane = forcefield(t,dt,step,plane,ks,kd,R,m);
     [X,Y,Z] = bindGrid(plane,plane_size);
     mesh(X,Y,Z);
-    xlim([0 plane_size(1)]);
-    ylim([0 plane_size(2)]);
-    zlim([-8 8]);
+    xlim(perspective);
+    ylim(perspective);
+    zlim(perspective);
     drawnow;
     %pause(0.01);
 end
